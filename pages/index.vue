e79b8728f2f14d8f46a2f122d6bba3e56ca33c2e
cimorexave <sadeq.s502@gmail.com>
2023-01-20 19:46:48 +0330

    map setup

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,21 @@
 definePageMeta({
     middleware: ['auth']
 })
+const L: any = useNuxtApp().$L
+let map: unknown
+let tileLayer: unknown
+// leaflet is a client side plugin
+// so it can only be used after mounted
+onMounted(() => {
+  let initialView: number[] = [35.706377, 51.382385]
+  map = L.map('map').setView(initialView, 11)
+  tileLayer = L.tileLayer(`https://tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+    attribution: '',
+    referrerPolicy: false,
+  }).addTo(map)
+  const markers = L.markerClusterGroup()
+})
 </script>
-<template>hi</template>
+<template>
+<div id="map"></div>
+</template>
