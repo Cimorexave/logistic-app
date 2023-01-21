ed16ae2ef59a2d366d914e4142b1b8fa68e494c2
cimorexave <sadeq.s502@gmail.com>
2023-01-21 17:39:51 +0330

    fix: css import module

--- plugins/leaflet.client.ts
+++ plugins/leaflet.client.ts
@@ -4,6 +4,6 @@ import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-draw'
-import '@/assets/sass/leaflet.draw.scss'
+import 'leaflet-draw/dist/leaflet.draw.css'
 const mCluster = markercluster
 export default defineNuxtPlugin((nuxtApp) => nuxtApp.provide('L', L))
