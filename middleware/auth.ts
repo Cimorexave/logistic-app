0ddbaa19354c189cd7286594879af2c00f075194
cimorexave <sadeq.s502@gmail.com>
2023-01-21 17:40:03 +0330

    updated atuh midd

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,16 +1,16 @@
 import { useAuth } from "~~/stores/auth"
 const isValid: boolean = false
+let cookieToken = useCookie('token').value
 export default defineNuxtRouteMiddleware(async () => {
-
     const user = useAuth().user
     if (!user) {
         // fetch user info
+        const {data, error, pending, refresh} = useFetch('')
+        //set token
     } else {
-        const cookieToken = useCookie('token').value
         // validate token
 
         // invalid
   }
-
   if (!isValid) navigateTo('/login')
 })
